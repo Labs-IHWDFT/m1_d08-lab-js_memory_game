51c745203c5604347b343bf5c91ef183714cc643
marcosantonastasi <marco.santonastasi@gmail.com>
2022-01-26 16:33:42 +0100

    fine

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+console.log("Memory index.js loaded")
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -24,3 +25,104 @@ const cards = [
   { name: 'the avengers', img: 'the-avengers.jpg' },
   { name: 'thor', img: 'thor.jpg' }
 ];
+
+let memoryGame = new MemoryGame(cards)
+
+const memoryBoard = document.getElementById("memory-board")
+
+const cardsHTML = memoryGame.cards.map(
+  card=>{
+  // These actions can only be taken when the page is completely loaded
+  /*
+   *
+   The following code creates a html element that has the following contents
+    <div class="card" data-card-name="${card.name}">
+      <div class="back" name="${card.img}"></div>
+      <div class="front" style="background: url(img/${card.img}) no-repeat"></div>
+    </div>
+  */
+
+    const outsideDiv = document.createElement("div")
+    outsideDiv.classList.add("card")
+    outsideDiv.setAttribute("data-card-name", card.name)
+
+    const insideDivBack = document.createElement("div")
+    insideDivBack.classList.add("back")
+    insideDivBack.name = card.img
+
+    const insideDivFront = document.createElement("div")
+    insideDivFront.classList.add("front")
+    insideDivFront.name = card.img
+    insideDivFront.style = `background: url(img/${card.img}) no-repeat` // same as HTM inline <div style= ...
+    
+    outsideDiv.appendChild(insideDivBack)
+    outsideDiv.appendChild(insideDivFront)
+
+    return outsideDiv
+  }
+)
+
+
+function flipCard(card){
+  card.classList.toggle("turned")
+}
+
+function setCardsToGuessed(card) {
+  card.classList.add('guessed');
+}
+
+function updateScoresBoard(score, clicked, guessed) {
+  document.getElementById('score').innerText = score;
+  document.getElementById('pairs-clicked').innerText = clicked;
+  document.getElementById('pairs-guessed').innerText = guessed;
+}
+
+
+function gameWon() {
+  const winOverlay = document.createElement('div');
+  const winBanner = document.createElement('h1');
+  winBanner.innerText = 'YOU HAVE SUPERPOWERS !';
+  winOverlay.appendChild(winBanner);
+  winOverlay.style.backgroundImage =
+    "url('https://wallpaperaccess.com/full/1135868.jpg')";
+  winOverlay.classList.add('win-modal');
+  document.querySelector('#memory-board').appendChild(winOverlay);
+}
+
+
+cardsHTML.forEach(cardHtml => {
+  cardHtml.addEventListener(
+    "click",
+
+    // ------ main game loop started by user click ---------
+    ( event )=>{
+      const clickedCard = event.currentTarget // ----- only way to know which div was clicked -------
+      flipCard(clickedCard)
+
+      const playResults = memoryGame.playCard( clickedCard )
+
+      // the following line is based on the fact that I would like to pass complex object from the game logic
+      // the object is: { isPair: false, playedCards: this.playedCards }
+      if(playResults.isPair){
+        playResults.playedCards.forEach(card=> setCardsToGuessed(card))
+      }else{
+        playResults.playedCards.forEach( card=>{
+          setTimeout(()=>flipCard(card), 1 * 1000)
+        })
+      }
+      
+        updateScoresBoard(
+          memoryGame.score,
+          memoryGame.clickedPairs,
+          memoryGame.guessedPairs
+        )
+
+        if(memoryGame.checkIfGameOver()) gameWon()
+
+    }
+    // ---------- end game loop --------
+  )
+
+  cardsHTML.forEach((cardHtml) => memoryBoard.appendChild(cardHtml));
+});
+
--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,63 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.score = 0;
+    this.guessedPairs = 0;
+    this.clickedPairs = 0;
+    this.playedCards = [];
+    this.shuffleCards();
   }
 
   shuffleCards() {
-    // ... write your code here
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
   }
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
+  checkIfPair() {
+    console.log(this.playedCards)
+    if(
+      // alternative syntax this.playedCards[0].getAttribute("data-card-name")
+      this.playedCards[0].dataset.cardName === this.playedCards[1].dataset.cardName
+    ){
+      return true
+    }
+    
   }
 
-  checkIfFinished() {
-    // ... write your code here
+  checkIfGameOver() {
+
+    // ------- add other logic here --------
+    // if (this.timer > 60 * 1000) return true
+    // if (this.clickedPairs > 5) return true
+    return this.guessedPairs === this.cards.length / 2
+  
+  }
+
+  playCard(card){
+    let playResults = { "isPair": false, "playedCards": [] }
+    if(this.playedCards.length < 2){
+      this.playedCards.push(card)
+    }
+    
+    if( this.playedCards.length === 2){
+      this.clickedPairs += 1
+      if(this.checkIfPair()){
+        playResults = { "isPair": true, "playedCards": this.playedCards }
+        this.score += 1
+        this.guessedPairs += 1
+        this.playedCards = [];
+      }else{
+        playResults = { "isPair": false, "playedCards": this.playedCards }
+        this.playedCards = [];
+      }
+    }
+    return playResults
   }
 }
+
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
